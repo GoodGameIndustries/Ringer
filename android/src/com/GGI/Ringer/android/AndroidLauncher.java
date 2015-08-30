1cb76c436bfde7a3ef42574cd129abec74b17acf
Emmett <goodgameindustries@gmail.com>
2015-08-30 16:34:55 -0500

    libgdx project set up

---
+++ android/src/com/GGI/Ringer/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.GGI.Ringer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.GGI.Ringer.Ringer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Ringer(), config);
+	}
+}
---
+++ desktop/src/com/GGI/Ringer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.GGI.Ringer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.GGI.Ringer.Ringer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ringer(), config);
+	}
+}
